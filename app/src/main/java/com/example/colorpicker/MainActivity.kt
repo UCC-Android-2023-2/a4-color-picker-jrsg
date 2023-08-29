54d11bfff12d4247eaff2a496de567c71798d322
Rubén Sánchez <rsanchez@ucc.mx>
2023-08-28 20:57:38 -0600

    SeekBar rojo funcionando

--- app/src/main/java/com/example/colorpicker/MainActivity.kt
+++ app/src/main/java/com/example/colorpicker/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.colorpicker
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rbFondo : RadioButton   = findViewById(R.id.rbFondo)
+        val rbFuente : RadioButton  = findViewById(R.id.rbFuente)
+
+        val sbRojo : SeekBar        = findViewById(R.id.sbRojo)
+        val sbVerde : SeekBar       = findViewById(R.id.sbVerde)
+        val sbAzul : SeekBar        = findViewById(R.id.sbAzul)
+
+        val tvMensaje : TextView    = findViewById(R.id.tvMensaje)
+
+        sbRojo.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+                if(rbFondo.isChecked) {
+                    tvMensaje.setBackgroundColor(Color.rgb(p1, 0, 0))
+                }
+
+                if(rbFuente.isChecked){
+                    tvMensaje.setTextColor(Color.rgb(p1, 0, 0))
+                }
+
+
+            }
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
     }
 }
